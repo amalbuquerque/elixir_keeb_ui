b2d11484dc1feb5d73d0dee98fdad18adc787439
amalbuquerque <andre.m.de.albuquerque@gmail.com>
2020-03-07 10:30:47 +0000

    Refactor the elixir_keeb.js module

--- assets/js/elixir_keeb.js
+++ assets/js/elixir_keeb.js
@@ -10,41 +10,48 @@ channel
 channel
   .push("get_layout", {})
   .receive("ok", ({layout}) => {
+    configureKeyboard(layout);
+  })
+
+
+const configureKeyboard = (layout) => {
+  const onChange = (input) => {
+    document.querySelector(".input").value = input;
+    console.log("Input changed", input);
+  }
+
+  const onKeyPress = (button) => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift(keyboard);
+  };
+
+  const handleShift = (keyboard) => {
+    let currentLayout = keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+  };
+
     let keyboard = new Keyboard({
+      newLineOnEnter: true,
+      tabCharOnTab: true,
       onChange: input => onChange(input),
       onKeyPress: button => onKeyPress(button),
       layout: layout
     });
 
     console.log("Keyboard", keyboard);
-  })
 
-/**
- * Update simple-keyboard when input is changed directly
- */
-document.querySelector(".input").addEventListener("input", event => {
-  keyboard.setInput(event.target.value);
-});
-
-function onChange(input) {
-  document.querySelector(".input").value = input;
-  console.log("Input changed", input);
-}
-
-function onKeyPress(button) {
-  console.log("Button pressed", button);
-
-  /**
-   * If you want to handle the shift and caps lock buttons
-   */
-  if (button === "{shift}" || button === "{lock}") handleShift();
-}
-
-function handleShift() {
-  let currentLayout = keyboard.options.layoutName;
-  let shiftToggle = currentLayout === "default" ? "shift" : "default";
-
-  keyboard.setOptions({
-    layoutName: shiftToggle
-  });
+    /**
+     * Update simple-keyboard when input is changed directly
+     */
+    document.querySelector(".input").addEventListener("input", event => {
+      keyboard.setInput(event.target.value);
+    });
 }
